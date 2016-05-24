2794162d7c9de73e69504232ae10d45232deebab
S.Percentage <Syn.Tri.Naga@gmail.com>
2016-05-25 00:05:27 +0900

    added .gitignore rules and flag textures from vanilla blocks

--- src/contents/flag.scala
+++ src/contents/flag.scala
@@ -6,12 +6,15 @@ package object flag
 {
 	import com.cterm2.tetra.ContentRegistry
 	import cpw.mods.fml.client.registry.{ClientRegistry, RenderingRegistry}
-	
+
 	var RenderID = 0
-	
+
 	def register
 	{
-		ContentRegistry register Block.setCreativeTab(CreativeTab) as "FlagBlock"
+		ContentRegistry register Block0.setCreativeTab(CreativeTab) in classOf[BlockSummoner] as "FlagBlock"
+		ContentRegistry register Block90 as "FlagBlock.90deg"
+		ContentRegistry register Block180 as "FlagBlock.180deg"
+		ContentRegistry register Block270 as "FlagBlock.270deg"
 		ContentRegistry register classOf[TileData] as "FlagTileData"
 	}
 	@SideOnly(Side.CLIENT)
@@ -26,33 +29,94 @@ package object flag
 package flag
 {
 	import net.{minecraft => mc}
-	import mc.block.BlockContainer, mc.block.material.Material
+	import mc.item.ItemBlockWithMetadata
+	import mc.block.{Block, BlockContainer}, mc.block.material.Material
+	import mc.world.World, mc.entity.player.EntityPlayer
 	import mc.tileentity.TileEntity, mc.client.renderer.tileentity.TileEntitySpecialRenderer
-	import mc.world.World
-	import mc.util.AxisAlignedBB, mc.entity.{Entity, EntityLivingBase}
+	import mc.util.{AxisAlignedBB, MathHelper}, mc.entity.{Entity, EntityLivingBase}
 	import mc.item.ItemStack
 	import scalaz._, Scalaz._
 	import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler
-	
+	import mc.init.Blocks
+
 	object Parameters
 	{
 		final val Space = 0.125f
 		final val InvSpace = 1.0f - Space
 		final val BaseHeight = 0.125f
-		final val Pole = 1.0f / 16.0f
+		final val Pole = 1.5f / 16.0f
 		final val FlagThickness = 0.75f / 16.0f
 	}
-	
-	object Block extends BlockContainer(Material.rock)
+
+	class BlockSummoner(block: Block) extends ItemBlockWithMetadata(block, block)
+	{
+		setMaxStackSize(1)
+		
+		override def onItemUse(stack: ItemStack, player: EntityPlayer, world: World, x: Int, y: Int, z: Int, side: Int,
+			hitX: Float, hitY: Float, hitZ: Float) =
+		{
+			val blk = (MathHelper.floor_double((player.rotationYaw / 90.0f).toDouble + 0.5d) & 3) match
+			{
+				case 0 => Block0
+				case 1 => Block90
+				case 2 => Block180
+				case 3 => Block270
+			}
+
+			// displacement check
+			val (finalY, finalZ, finalX, finalSide) = world.getBlock(x, y, z) match
+			{
+				case Blocks.snow if (world.getBlockMetadata(x, y, z) & 7) < 1 => (y, z, x, 1)
+				case Blocks.vine | Blocks.tallgrass | Blocks.deadbush => (y, z, x, side)
+				case b: Block if b.isReplaceable(world, x, y, z) => (y, z, x, side)
+				case _ =>
+				(
+					if(side == 0) y - 1 else if(side == 1) y + 1 else y,
+					if(side == 2) z - 1 else if(side == 3) z + 1 else z,
+					if(side == 4) x - 1 else if(side == 5) x + 1 else x,
+					side
+				)
+			}
+
+			// validation
+			val stackHasItem = stack.stackSize > 0
+			val playerEditable = player.canPlayerEdit(finalX, finalY, finalZ, finalSide, stack)
+			val heightLimitation = finalY >= 255
+			val isSolidBlock = blk.getMaterial.isSolid
+			val canPlaceEntityOnSide = world.canPlaceEntityOnSide(blk, finalX, finalY, finalZ, false, finalSide, player, stack)
+			if(stackHasItem && playerEditable && !(heightLimitation && isSolidBlock) && canPlaceEntityOnSide)
+			{
+				val meta = this.getMetadata(stack.getItemDamage)
+				val actionResult = blk.onBlockPlaced(world, finalX, finalY, finalZ, finalSide, hitX, hitY, hitZ, meta)
+
+				if(world.setBlock(finalX, finalY, finalZ, blk, actionResult, 3))
+				{
+					if(world.getBlock(finalX, finalY, finalZ) == blk)
+					{
+						blk.onBlockPlacedBy(world, finalX, finalY, finalZ, player, stack)
+						blk.onPostBlockPlaced(world, finalX, finalY, finalZ, actionResult)
+					}
+
+					world.playSoundEffect((finalX.toFloat + 0.5f).toDouble, (finalY.toFloat + 0.5f).toDouble, (finalZ.toFloat + 0.5f).toDouble,
+						blk.stepSound.func_150496_b(), (blk.stepSound.getVolume + 1.0f) / 2.0f, blk.stepSound.getPitch * 0.8f)
+					stack.stackSize = stack.stackSize - 1
+				}
+				true
+			}
+			else false
+		}
+	}
+	sealed abstract class AbstractBlock extends BlockContainer(Material.rock)
 	{
 		this.setHardness(1.0f)
 		this.setBlockBounds(Parameters.Space, 0.0f, Parameters.Space, Parameters.InvSpace, 1.0f, Parameters.InvSpace)
-		
+
 		override val isOpaqueCube = false
 		override val isNormalCube = false
 		override val renderAsNormalBlock = false
-		override def getRenderType() = RenderID
-		
+		override def getRenderType = RenderID
+		override def getIcon(side: Int, meta: Int) = Blocks.planks.getIcon(side, 0)
+
 		// Collision Box for Visibility Testing(getCollisionBoundingBoxFromPool)
 		// Collision Test against Entities
 		override def addCollisionBoxesToList(world: World, x: Int, y: Int, z: Int, mask: AxisAlignedBB, boxes: java.util.List[_], entity: Entity)
@@ -64,44 +128,41 @@ package flag
 				case _ => /* Nothing to do */
 			}
 		}
-		
+
 		override def createNewTileEntity(world: World, meta: Int) = new TileData
-		
+
 		override def onBlockPlacedBy(world: World, x: Int, y: Int, z: Int, actor: EntityLivingBase, stack: ItemStack)
 		{
 			if(!world.isRemote)
 			{
 				if(stack.hasDisplayName)
 				{
-					// ModInstance.logger.info("onBlockPlacedBy with DisplayName({})", stack.getDisplayName)
 					for(x <- Option(world.getTileEntity(x, y, z)) >>= (x => Option(x.asInstanceOf[TileData])))
 					{
 						x.name = Some(stack.getDisplayName)
 					}
 				}
-				else
-				{
-					// ModInstance.logger.info("onBlockPlacedBy")
-				}
-			}
-			else
-			{
-				// Fetch TileData from Server
+				// Sync TileData from Server
 				world.markBlockForUpdate(x, y, z)
 			}
 		}
 	}
+	object Block0 extends AbstractBlock
+	object Block90 extends AbstractBlock
+	object Block180 extends AbstractBlock
+	object Block270 extends AbstractBlock
+
 	class TileData extends TileEntity
 	{
 		import mc.nbt.NBTTagCompound
 		import com.cterm2.tetra.ActiveNBTRecord._
 		import mc.network.play.server.S35PacketUpdateTileEntity
 		import mc.network.NetworkManager
-		
+
 		var name: Option[String] = None
-		
+
 		def hasCustomName = this.name.isDefined
-		
+
 		override def writeToNBT(tag: NBTTagCompound)
 		{
 			super.writeToNBT(tag)
@@ -131,109 +192,105 @@ package flag
 	object BlockRenderer extends ISimpleBlockRenderingHandler
 	{
 		import mc.block.Block
-		import mc.client.renderer.{RenderBlocks, Tessellator}
+		import mc.client.renderer.{RenderBlocks, Tessellator, EntityRenderer}
 		import mc.world.IBlockAccess
 		import org.lwjgl.opengl.GL11._
-		
+		import com.cterm2.tetra.StaticMeshData._
+		import mc.client.Minecraft
+
+		implicit class MeshConstructorHelper(val mesh: StaticMesh) extends AnyVal
+		{
+			def renderBase() = mesh.setRenderBounds(Parameters.Space, 0.0f, Parameters.Space, Parameters.InvSpace, Parameters.BaseHeight, Parameters.InvSpace).
+				renderFaceYPos.renderFaceXZ
+			def renderPole() = mesh.setRenderBounds(0.5f - Parameters.Pole, Parameters.BaseHeight, 0.5f - Parameters.Pole, 0.5f + Parameters.Pole, 1.0f, 0.5f + Parameters.Pole).
+				renderFaceXZ
+			def renderFlagPart(n: Int) = mesh.setRenderBounds(0.5f + Parameters.Pole + n * 1.25f / 16.0f, 1.0f - (3.0f + 1.0f + 1.0f + 3.0f - n) / 16.0f, 0.5f - Parameters.FlagThickness,
+				0.5f + Parameters.Pole + (1 + n) * 1.25f / 16.0f, 1.0f - (n + 1.0f) / 16.0f, 0.5f + Parameters.FlagThickness).
+				renderFaceYPos.renderFaceYNeg.renderFaceXPos.renderFaceZNeg.renderFaceZPos
+			def renderFlagPart90(n: Int) = mesh.setRenderBounds(0.5f - Parameters.FlagThickness, 1.0f - (3.0f + 1.0f + 1.0f + 3.0f - n) / 16.0f, 0.5f + Parameters.Pole + n * 1.25f / 16.0f,
+				0.5f + Parameters.FlagThickness, 1.0f - (n + 1.0f) / 16.0f, 0.5f + Parameters.Pole + (1 + n) * 1.25f / 16.0f).
+				renderFaceYPos.renderFaceYNeg.renderFaceZPos.renderFaceXNeg.renderFaceXPos
+			def renderFlagPartInv(n: Int) = mesh.setRenderBounds(0.5f - Parameters.Pole - (n + 1.0f) * 1.25f / 16.0f, 1.0f - (3.0f + 1.0f + 1.0f + 3.0f - n) / 16.0f, 0.5f - Parameters.FlagThickness,
+				0.5f - Parameters.Pole - n * 1.25f / 16.0f, 1.0f - (n + 1.0f) / 16.0f, 0.5f + Parameters.FlagThickness).
+				renderFaceYPos.renderFaceYNeg.renderFaceXNeg.renderFaceZNeg.renderFaceZPos
+			def renderFlagPart90Inv(n: Int) = mesh.setRenderBounds(0.5f - Parameters.FlagThickness, 1.0f - (3.0f + 1.0f + 1.0f + 3.0f - n) / 16.0f, 0.5f - Parameters.Pole - (n + 1.0f) * 1.25f / 16.0f,
+				0.5f + Parameters.FlagThickness, 1.0f - (n + 1.0f) / 16.0f, 0.5f - Parameters.Pole - n * 1.25f / 16.0f).
+				renderFaceYPos.renderFaceYNeg.renderFaceZNeg.renderFaceXNeg.renderFaceXPos
+		}
+		val meshBase = EmptyStaticMesh.renderBase
+		val meshPole = EmptyStaticMesh.renderPole
+		val meshBase2 = EmptyStaticMesh.renderBase.renderFaceYNeg
+		val meshPole2 = EmptyStaticMesh.renderPole.renderFaceYPos
+		val meshdata = EmptyStaticMesh.renderFlagPart(0).renderFlagPart(1).renderFlagPart(2).renderFlagPart(3)
+		val meshdata90 = EmptyStaticMesh.renderFlagPart90(0).renderFlagPart90(1).renderFlagPart90(2).renderFlagPart90(3)
+		val meshdata180 = EmptyStaticMesh.renderFlagPartInv(0).renderFlagPartInv(1).renderFlagPartInv(2).renderFlagPartInv(3)
+		val meshdata270 = EmptyStaticMesh.renderFlagPart90Inv(0).renderFlagPart90Inv(1).renderFlagPart90Inv(2).renderFlagPart90Inv(3)
+		val meshdata2 = EmptyStaticMesh.renderFlagPart(0).renderFlagPart(1).renderFlagPart(2).renderFlagPart(3)
+
 		override def getRenderId() = RenderID
 		override def shouldRender3DInInventory(model: Int) = true
 		override def renderInventoryBlock(block: Block, meta: Int, model: Int, renderer: RenderBlocks)
 		{
-			// render plate
 			glPushMatrix()
 			glRotatef(90.0f, 0.0f, 1.0f, 0.0f)
+			glScalef(1.25f, 1.25f, 1.25f)
 			glTranslatef(-0.5f, -0.5f, -0.5f)
-			renderer.setRenderBounds(Parameters.Space, 0.0f, Parameters.Space, Parameters.InvSpace, Parameters.BaseHeight, Parameters.InvSpace)
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceYNeg(block, 0, 0, 0, block.getBlockTextureFromSide(0))
-			Tessellator.instance.draw()
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceYPos(block, 0, 0, 0, block.getBlockTextureFromSide(1))
-			Tessellator.instance.draw()
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceZNeg(block, 0, 0, 0, block.getBlockTextureFromSide(2))
-			Tessellator.instance.draw()
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceZPos(block, 0, 0, 0, block.getBlockTextureFromSide(3))
-			Tessellator.instance.draw()
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceXNeg(block, 0, 0, 0, block.getBlockTextureFromSide(4))
-			Tessellator.instance.draw()
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceXPos(block, 0, 0, 0, block.getBlockTextureFromSide(5))
-			Tessellator.instance.draw()
-			// render pole
-			renderer.setRenderBounds(0.5f - Parameters.Pole, Parameters.BaseHeight, 0.5f - Parameters.Pole,
-				0.5f + Parameters.Pole, 1.0f, 0.5f + Parameters.Pole)
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceYPos(block, 0, 0, 0, block.getBlockTextureFromSide(1))
-			Tessellator.instance.draw()
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceZNeg(block, 0, 0, 0, block.getBlockTextureFromSide(2))
-			Tessellator.instance.draw()
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceZPos(block, 0, 0, 0, block.getBlockTextureFromSide(3))
-			Tessellator.instance.draw()
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceXNeg(block, 0, 0, 0, block.getBlockTextureFromSide(4))
-			Tessellator.instance.draw()
-			Tessellator.instance.startDrawingQuads()
-			renderer.renderFaceXPos(block, 0, 0, 0, block.getBlockTextureFromSide(5))
-			// render flag
-			for(i <- 0 until 4)
-			{
-				renderer.setRenderBounds(0.5f + Parameters.Pole + i * 1.25f / 16.0f, 1.0f - (8.0f - i) / 16.0f, 0.5f - Parameters.FlagThickness,
-					0.5f + Parameters.Pole + (1.0f + i) * 1.25f / 16.0f, 1.0f - (1.0f + i) / 16.0f, 0.5f + Parameters.FlagThickness)
-				renderer.renderFaceYNeg(block, 0, 0, 0, block.getBlockTextureFromSide(0))
-				renderer.renderFaceYPos(block, 0, 0, 0, block.getBlockTextureFromSide(1))
-				renderer.renderFaceZNeg(block, 0, 0, 0, block.getBlockTextureFromSide(2))
-				renderer.renderFaceZPos(block, 0, 0, 0, block.getBlockTextureFromSide(3))
-				renderer.renderFaceXPos(block, 0, 0, 0, block.getBlockTextureFromSide(5))
-			}
-			Tessellator.instance.draw()
+			meshBase2.renderWithNormals(Blocks.stone, renderer)
+			meshPole2.renderWithNormals(Blocks.planks, renderer)
+			meshdata2.renderWithNormals(Blocks.wool, renderer)
 			glPopMatrix()
 		}
 		override def renderWorldBlock(world: IBlockAccess, x: Int, y: Int, z: Int, block: Block, model: Int, renderer: RenderBlocks) =
 		{
-			// render plate
-			renderer.setRenderBounds(Parameters.Space, 0.0f, Parameters.Space, Parameters.InvSpace, Parameters.BaseHeight, Parameters.InvSpace)
-			renderer.renderFaceYNeg(block, x, y, z, block.getBlockTextureFromSide(0))
-			renderer.renderFaceYPos(block, x, y, z, block.getBlockTextureFromSide(1))
-			renderer.renderFaceZNeg(block, x, y, z, block.getBlockTextureFromSide(2))
-			renderer.renderFaceZPos(block, x, y, z, block.getBlockTextureFromSide(3))
-			renderer.renderFaceXNeg(block, x, y, z, block.getBlockTextureFromSide(4))
-			renderer.renderFaceXPos(block, x, y, z, block.getBlockTextureFromSide(5))
-			// render pole
-			renderer.setRenderBounds(0.5f - Parameters.Pole, Parameters.BaseHeight, 0.5f - Parameters.Pole,
-				0.5f + Parameters.Pole, 1.0f, 0.5f + Parameters.Pole)
-			renderer.renderFaceYPos(block, x, y, z, block.getBlockTextureFromSide(1))
-			renderer.renderFaceZNeg(block, x, y, z, block.getBlockTextureFromSide(2))
-			renderer.renderFaceZPos(block, x, y, z, block.getBlockTextureFromSide(3))
-			renderer.renderFaceXNeg(block, x, y, z, block.getBlockTextureFromSide(4))
-			renderer.renderFaceXPos(block, x, y, z, block.getBlockTextureFromSide(5))
-			// render flag
-			for(i <- 0 until 4)
+			// Color Multiplier
+			val cm = block.colorMultiplier(world, x, y, z)
+			val (baseR, baseG, baseB) = ((cm >> 16 & 255).toFloat / 255.0f, (cm >> 8 & 255).toFloat / 255.0f, (cm & 255).toFloat / 255.0f)
+			val (r, g, b) = if(EntityRenderer.anaglyphEnable) ((baseR * 30.0f + baseG * 59.0f + baseB * 11.0f) / 100.0f, (baseR * 30.0f + baseG * 70.0f) / 100.0f, (baseR * 30.0f + baseB * 70.0f) / 100.0f)
+				else (baseR, baseG, baseB)
+
+			// Render with Color Multiplier
+			val tess = Tessellator.instance
+			val (upLight, xLight, zLight) = (0.5f, 0.8f, 0.6f)
+
+			// Render Base Bottom
+			if(renderer.renderAllFaces || block.shouldSideBeRendered(world, x, y - 1, z, 0))
 			{
-				renderer.setRenderBounds(0.5f + Parameters.Pole + i * 1.25f / 16.0f, 1.0f - (8.0f - i) / 16.0f, 0.5f - Parameters.FlagThickness,
-					0.5f + Parameters.Pole + (1.0f + i) * 1.25f / 16.0f, 1.0f - (1.0f + i) / 16.0f, 0.5f + Parameters.FlagThickness)
-				renderer.renderFaceYNeg(block, x, y, z, block.getBlockTextureFromSide(0))
-				renderer.renderFaceYPos(block, x, y, z, block.getBlockTextureFromSide(1))
-				renderer.renderFaceZNeg(block, x, y, z, block.getBlockTextureFromSide(2))
-				renderer.renderFaceZPos(block, x, y, z, block.getBlockTextureFromSide(3))
-				renderer.renderFaceXPos(block, x, y, z, block.getBlockTextureFromSide(5))
+				tess.setBrightness(block.getMixedBrightnessForBlock(world, x, y - 1, z))
+				tess.setColorOpaque_F(upLight * r, upLight * g, upLight * b)
+				renderer.renderFaceYNeg(block, x, y, z, Blocks.stone.getBlockTextureFromSide(0))
 			}
-			false
+			// Render Pole Top
+			if(renderer.renderAllFaces || block.shouldSideBeRendered(world, x, y + 1, z, 1))
+			{
+				renderer.setRenderBounds(0.5f - Parameters.Pole, Parameters.BaseHeight, 0.5f - Parameters.Pole, 0.5f + Parameters.Pole, 1.0f, 0.5f + Parameters.Pole)
+				tess.setBrightness(block.getMixedBrightnessForBlock(world, x, y + 1, z))
+				tess.setColorOpaque_F(r, g, b)
+				renderer.renderFaceYPos(block, x, y, z, Blocks.planks.getBlockTextureFromSide(1))
+			}
+
+			// Render static meshes
+			meshBase.render(world, Blocks.stone, x, y, z, renderer, upLight, xLight, zLight, r, g, b)
+			meshPole.render(world, Blocks.planks, x, y, z, renderer, upLight, xLight, zLight, r, g, b)
+			val mesh = block match
+			{
+				case Block0 => meshdata
+				case Block90 => meshdata90
+				case Block180 => meshdata180
+				case Block270 => meshdata270
+			}
+			mesh.render(world, Blocks.wool, x, y, z, renderer, upLight, xLight, zLight, r, g, b)
+			true
 		}
 	}
 	object TERenderer extends TileEntitySpecialRenderer
 	{
 		import org.lwjgl.opengl.GL11._
 		import mc.client.renderer.Tessellator
-		
-		val baseVertices = 
+
+		val baseVertices =
 		{
 			val tess = new Tessellator
-			
+
 			tess.setColorRGBA_F(0.0f, 0.0f, 0.0f , 0.375f)
 			tess.addVertex(0.0d, 0.0d, 0.0d)
 			tess.addVertex(0.0d, 1.0d, 0.0d)
@@ -241,19 +298,19 @@ package flag
 			tess.addVertex(1.0d, 0.0d, 0.0d)
 			tess.getVertexState(0.0f, 0.0f, 0.0f)
 		}
-		
+
 		override def renderTileEntityAt(tile: TileEntity, x: Double, y: Double, z: Double, p: Float)
 		{
 			val viewEntity = this.field_147501_a.field_147551_g
 			val fontRender = this.func_147498_b()
-			val contentStr = tile.asInstanceOf[TileData].name | Block.getLocalizedName()
+			val contentStr = tile.asInstanceOf[TileData].name | Block0.getLocalizedName()
 			val contentMetrics = fontRender.getStringWidth(contentStr)
 			val pixelScaling = 1.6f / 60.0f
-			
+
 			def renderNameBase()
 			{
 				val tess = Tessellator.instance
-				
+
 				tess.startDrawingQuads()
 				tess.setVertexState(this.baseVertices)
 				tess.draw()
@@ -277,7 +334,7 @@ package flag
 				glDisable(GL_BLEND)
 				Seq(GL_DEPTH_TEST, GL_CULL_FACE, GL_LIGHTING) foreach glEnable
 			}
-			
+
 			glPushMatrix()
 			glTranslated(x + 0.5d, y + 1.0d, z + 0.5d)
 			glRotatef(-viewEntity.rotationYaw, 0.0f, 1.0f, 0.0f)
